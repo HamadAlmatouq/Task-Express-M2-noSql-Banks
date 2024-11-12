28a23687d148bcebd7a5b27a04d9d8279e5c15a8
Hamad Almatouq <hamad.almatouq@outlook.com>
2024-11-12 10:36:13 +0300

    Extra task

--- models/Accounts.js
+++ models/Accounts.js
@@ -2,7 +2,7 @@ const { model, Schema } = require("mongoose");
 
 const AccountsSchema = new Schema({
   username: { type: String, required: true },
-  funds: { type: Number },
+  funds: { type: Number, default: 0 },
 });
 
 module.exports = model("Account", AccountsSchema);
